91006f0b2b632a833760cfe1722cab376f2c257b
kidd1414 <torreskiddandrei.bsit@gmail.com>
2025-04-09 02:56:35 +0800

    Add files via upload

---
+++ CodeReviser/replit.nix
@@ -0,0 +1,30 @@
+{pkgs}: {
+  deps = [
+    pkgs.tesseract
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.glibcLocales
+    pkgs.libGLU
+    pkgs.libGL
+    pkgs.poppler_utils
+    pkgs.xcbuild
+    pkgs.swig
+    pkgs.openjpeg
+    pkgs.mupdf
+    pkgs.libjpeg_turbo
+    pkgs.jbig2dec
+    pkgs.harfbuzz
+    pkgs.gumbo
+    pkgs.freetype
+    pkgs.hdf5
+    pkgs.postgresql
+    pkgs.openssl
+  ];
+}
